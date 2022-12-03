5a2fd86418be2cd8470bb74819181bb4193ef777
lataf <alexfatkin@gmail.com>
2022-12-03 18:05:55 +0300

    add git_work

---
+++ Teach/git_work.py
@@ -0,0 +1,14 @@
+"""
+@author: lataf 
+@file: git_work.py 
+@time: 02.12.2022 21:52
+Модуль отвечает за 
+UML схема модуля
+Сценарий работы модуля:
+Тест модуля находится в папке model/tests.
+"""
+
+# from git import Repo
+
+if __name__ == '__main__':
+    print('Hi, Python')
